776e7a1bd641b6e2ae026eee4e29748fdcddac57
banux <banux@odin.(none)>
2009-08-14 21:50:27 +0200

    first import

---
+++ rt.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'twitter'
+require 'eventmachine'
+require 'pp'
+
+user = "banux_hartest"
+passwd = "toto42"
+
+last_id = 0
+
+httpauth = Twitter::HTTPAuth.new(user, passwd)
+base = Twitter::Base.new(httpauth)
+
+EM.run do
+   EM.add_periodic_timer(2) do
+    if last_id == 0
+      res = Twitter::Search.new('#har2009').fetch().results
+    else
+      res = Twitter::Search.new('#har2009').since(last_id).fetch().results    
+    end
+    if (res.size > 0)
+    last_id = res.first.id    
+    res.each do |t|
+          base.update("RT @" + t.from_user + " " + t.text)        
+    end
+  end
+  end
+end
